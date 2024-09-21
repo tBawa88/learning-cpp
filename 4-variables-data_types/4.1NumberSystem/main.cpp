e9e29eeecdfb2bd5d385acb0986d0e5e865a869b
tbawa88 <tbawa3388@gmail.com>
2024-09-21 16:55:27 +0530

    renamed

---
+++ 4-variables-data_types/4.1NumberSystem/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+int main () {
+    int decimal = 15;
+    int hexadecimal = 0x0f;
+    int binary = 0b1111;
+
+    std::cout << "Decimal number = " << decimal << std::endl;
+    std::cout << "Decimal number = " << hexadecimal << std::endl;
+    std::cout << "Decimal number = " << binary << std::endl;
+
+    return 0;
+}
